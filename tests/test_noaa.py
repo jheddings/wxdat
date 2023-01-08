b776fc051463f989f5609edd6c887c29dd572a9c
jheddings <jason@heddings.com>
2023-01-07 19:49:51 -0700

    Added good station test.

--- tests/test_noaa.py
+++ tests/test_noaa.py
@@ -10,3 +10,12 @@ def test_bad_station():
     station = noaa.Station("Bad Station", station="XXXX")
 
     assert station.current_conditions is None
+
+
+@pytest.mark.vcr()
+def test_denver_station():
+    station = noaa.Station("Denver Airport", station="KDEN")
+    cond = station.current_conditions
+
+    assert cond is not None
+    assert cond.station_id == "KDEN"
